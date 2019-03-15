e6b9f66a625bbc1f06ae728f62c9cc7de41747a3
DindaKartika <dkartika0887@gmail.com>
2019-03-15 08:06:20 +0700

    Fitur Rule

--- src/page/rule.js
+++ src/page/rule.js
@@ -8,7 +8,7 @@ class Rule extends Component {
         return (
             <div className="rule">
                 <section>
-                <h4 className="title">How to play:</h4>
+                <h4 className="titlerule">How to play:</h4>
                     <h5>You only have 3 opportunities to answer</h5>
                     <h5>Choose all words similar to the word above</h5>
                     <h5>Every true answer will give you more points</h5>
